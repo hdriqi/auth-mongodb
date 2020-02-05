f0968cf9cd372210ff670a278dc6593624eb1986
hdriqi <ra@evius.id>
2020-02-05 12:10:43 +0700

    add PORT in env var

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const RedisStore = require('rate-limit-redis')
 
 const Controller = require('./controller')
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 const app = express()
 const ctl = new Controller()
